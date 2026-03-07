0c6ebd9f2bb476603ad55c5ebe1db00b52a68a06
vansh-verma-dev <vanshverma6065@gmail.com>
2026-03-07 22:00:59 +0530

    initial add js destinatin deatils for btn click event using js

--- main.js
+++ main.js
@@ -1,25 +1,73 @@
-let heading = document.getElementById("destination-heding");
-let rishikeshbtn = document.getElementById('rishikeshbokkingBtn');
+ 
 
-
-
-
-rishikeshbtn.addEventListener('click',()=>{
-  let destinations = {
+ // destination data
+const destinations = {
   rishikesh: {
-    heading: "Explore Rishekesh",
-    subheading:"Adventure capital of India. Experience river rafting, temples and mountains",
+    heading: "Explore Rishikesh",
+    subheading: "Adventure capital of India. River rafting, temples and mountains.",
+    image : "https://i.pinimg.com/736x/76/58/5e/76585ed6de36da625effa911b9700573.jpg"
   },
+
   manali: {
-     heading: "Explore Manali",
-    subheading:"Adventure capital of India. Experience river rafting, temples and mountains",
+    heading: "Explore Manali",
+    subheading: "Beautiful hill station with snow mountains and adventure sports.",
+    image: "https://i.pinimg.com/1200x/45/e5/83/45e583fbc1d4c327576449b49bdb018b.jpg"
+  },
+
+  kashmir: {
+    heading: "Explore Kashmir",
+    subheading: "Paradise on earth with lakes and valleys.",
+    image : "https://i.pinimg.com/1200x/5a/85/2b/5a852bd548e3f92e4e76705503742ff1.jpg"
+  },
+  Goa: {
+    heading:  "explore Goa",
+    subheading : "Paradise on earth with lakes and valleys.",
+    image : "https://i.pinimg.com/736x/e4/a7/3a/e4a73a0e728e8897cfa98367d47166c5.jpg"
   }
 };
-  window.location = "tripbook.html";
- heading.innerHTML = destinations.rishikesh.heading;
-})
 
 
+// buttons (index page)
+let rishikeshBtn = document.getElementById("rishikeshbokkingBtn");
+let manaliBtn = document.getElementById("manaliBtn");
+let kashmirBtn = document.getElementById("kashmirBtn");
+
+if (rishikeshBtn) {
+  rishikeshBtn.addEventListener("click", () => {
+    window.location.href = "tripbook.html?place=rishikesh";
+  });
+}
+
+if (manaliBtn) {
+  manaliBtn.addEventListener("click", () => {
+    window.location.href = "tripbook.html?place=manali";
+  });
+}
+
+if (kashmirBtn) {
+  kashmirBtn.addEventListener("click", () => {
+    window.location.href = "tripbook.html?place=kashmir";
+  });
+}
+
+
+// tripbook page
+let heading = document.getElementById("destination-heding");
+let subheading = document.getElementById("subheading");
+let Images = document.getElementById("destination-image");
+
+if (heading) {
+
+  let params = new URLSearchParams(window.location.search);
+  let place = params.get("place");
+
+  if (destinations[place]) {
+    heading.innerHTML = destinations[place].heading;
+    subheading.innerHTML = destinations[place].subheading;
+    Images.src = destinations[place].image
+  }
+
+}
 
 
 
