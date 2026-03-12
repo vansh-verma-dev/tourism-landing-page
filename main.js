229a7f741f7065320bbb071a3c0d2a89937695b0
vansh-verma-dev <vanshverma6065@gmail.com>
2026-03-12 10:45:30 +0530

    add payemt page

--- main.js
+++ main.js
@@ -189,3 +189,28 @@ changeImage();
 
 
 // ===========================================
+const steps = document.querySelectorAll(".step");
+const formSteps = document.querySelectorAll(".form-step");
+const nextBtns = document.querySelectorAll(".next-btn");
+
+let currentStep = 0;
+
+nextBtns.forEach(btn => {
+
+btn.addEventListener("click", () => {
+
+formSteps[currentStep].classList.remove("active");
+steps[currentStep].classList.remove("active");
+
+currentStep++;
+
+if(currentStep >= formSteps.length){
+currentStep = formSteps.length - 1;
+}
+
+formSteps[currentStep].classList.add("active");
+steps[currentStep].classList.add("active");
+
+});
+
+});
